b4bec1995b28b0dc776db3d2a46e911398f5d955
Marcel Heidebrecht <marcel@heidebrecht.de>
2026-06-15 11:53:03 +0200

    build: migrate setup.sh to vanilla TRL stack (drop unsloth)

--- setup.sh
+++ setup.sh
@@ -12,4 +12,8 @@ fi
 
 uv venv .venv --python 3.12
 source .venv/bin/activate
-uv pip install unsloth https://github.com/vllm-project/vllm/releases/download/v0.19.1/vllm-0.19.1+cu130-cp38-abi3-manylinux_2_35_x86_64.whl ipykernel ipywidgets scipy matplotlib --torch-backend=auto
+uv pip install \
+  "trl>=0.26" peft bitsandbytes accelerate \
+  https://github.com/vllm-project/vllm/releases/download/v0.19.1/vllm-0.19.1+cu130-cp38-abi3-manylinux_2_35_x86_64.whl \
+  datasets scipy matplotlib ipywidgets \
+  --torch-backend=auto
